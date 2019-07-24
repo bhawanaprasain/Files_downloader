fef371b72cbca48a7621c395ac1298fd1e5e1245
bhawana <bhawana.prs@gmail.com>
2019-07-24 23:41:05 +0545

    Download movie in Downloads/ folder as movie.mp4

--- api.js
+++ api.js
@@ -1,64 +1,73 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
+var download = require("download-file");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 var XMLHttpRequest = require("node-http-xhr");
 const Http = new XMLHttpRequest();
 
-  readline.question("What's your favourite movie?", (name) => {
-    const url = `https://yts.lt/api/v2/list_movies.json?query_term=${name}`;
-    Http.addEventListener('load', function(){
-  
-        var jsonObject = JSON.parse(Http.response);
-      
-        var id = jsonObject.data.movies[0].id
-        jsonObject.data.movies[0].id
-        const idURL = `https://yts.lt/api/v2/movie_reviews.json?movie_id=${id}`;
-        Http.open("GET" , idURL);
-        var jsonObjectDetail = JSON.parse(Http.response);
+readline.question("What's your favourite movie?", name => {
+  const url = `https://yts.lt/api/v2/list_movies.json?query_term=${name}`;
+  Http.addEventListener("load", function() {
+    var jsonObject = JSON.parse(Http.response);
 
-        var movieFormat = jsonObjectDetail.data.movies[0].torrents;
-        var qualityList = [];
-        maxQuality = 0;
+    var id = jsonObject.data.movies[0].id;
+    jsonObject.data.movies[0].id;
+    const idURL = `https://yts.lt/api/v2/movie_reviews.json?movie_id=${id}`;
+    Http.open("GET", idURL);
+    var jsonObjectDetail = JSON.parse(Http.response);
 
-      movieFormat.forEach(sample => {
+    var movieFormat = jsonObjectDetail.data.movies[0].torrents;
+    var qualityList = [];
+    maxQuality = 0;
 
-        var movieQuality = sample.quality;
-        qualityList.push(movieQuality);
-        slicedArray = [];
-        maxQuality = 0;
-        var torrentObject ={};
-        qualityList.forEach(quality=>{
-          var slicedQuality = quality.slice(0, -1);
-        
-        slicedArray.push(slicedQuality);
-          if(maxQuality< Number(slicedQuality) ){
-            maxQuality = Number(slicedQuality);
-            torrentObject = sample;
-          }
+    movieFormat.forEach(sample => {
+      var movieQuality = sample.quality;
+      qualityList.push(movieQuality);
+      maxQuality = 0;
+      var torrentObject = {};
+      qualityList.forEach(quality => {
+        var slicedQuality = quality.slice(0, -1);
 
-         });
-        
-         console.log(torrentObject);
-         console.log(torrentObject.url)
-   
+        if (maxQuality < Number(slicedQuality)) {
+          maxQuality = Number(slicedQuality);
+          torrentObject = sample;
+        }
       });
-    
-        const downloadQuery = require('readline').createInterface({
+
+      console.log(torrentObject);
+      console.log(torrentObject.url);
+      function downloadMovie() {
+        var downloadURL = torrentObject.url;
+        var options = {
+          directory: "../../Downloads/",
+          filename: "name.mp4"
+        };
+        download(downloadURL, options, function(err) {
+          if (err) throw err;
+          console.log("wow");
+        });
+      }
+
+      const downloadQuery = require("readline").createInterface({
         input: process.stdin,
         output: process.stdout
       });
-      downloadQuery.question("Do you want to download the torrent file with maximum quality?", (ans) => {
-        if(ans == "y"){
-          console.log("Download in progress");
-        }});
-    
+      downloadQuery.question(
+        "Do you want to download the torrent file with maximum quality?",
+        ans => {
+          if (ans == "y") {
+            console.log("Download in progress");
+            downloadMovie();
+          }
+        }
+      );
     });
-   
-    Http.open("GET" , url);
+  });
 
-    Http.send();
-    readline.close()
-  })
-  
+  Http.open("GET", url);
+
+  Http.send();
+  readline.close();
+});
